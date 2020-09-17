1bb36b14c46bd0e9d8ff855d879c55090d2abaa8
FacundoMartinezCampos <facundocamposmartinez@hotmail.com>
2020-09-17 12:48:07 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Cards, Chart, CardsCalculation } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import Typography from '@material-ui/core/Typography';
 
 class App extends React.Component {
 state = {
@@ -19,6 +20,9 @@ state = {
 
     return (
       <div className={styles.container}>
+        <Typography variant="h1">
+          COVID-19
+        </Typography>
         <Cards data={this.state.data} />
         <CardsCalculation data={this.state.data} />
         <Chart />
